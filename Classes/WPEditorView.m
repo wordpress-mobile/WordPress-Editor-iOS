995bc0dfaf4ea1b74a5ee7d2d6277f0e3597a29a
Diego Rey Mendez <diegoreymendez@gmail.com>
2014-09-08 09:47:52 -0300

    Adds slashes before inserting any HTML to prevent JS injections.

--- Classes/WPEditorView.m
+++ Classes/WPEditorView.m
@@ -471,13 +471,13 @@ shouldStartLoadWithRequest:(NSURLRequest *)request
 
 #pragma mark - URL & HTML utilities
 
-- (NSString *)removeQuotesFromHTML:(NSString *)html
+- (NSString *)addSlashes:(NSString *)html
 {
+    html = [html stringByReplacingOccurrencesOfString:@"\\" withString:@"\\\\"];
     html = [html stringByReplacingOccurrencesOfString:@"\"" withString:@"\\\""];
-    html = [html stringByReplacingOccurrencesOfString:@"“" withString:@"&quot;"];
-    html = [html stringByReplacingOccurrencesOfString:@"”" withString:@"&quot;"];
     html = [html stringByReplacingOccurrencesOfString:@"\r"  withString:@"\\r"];
     html = [html stringByReplacingOccurrencesOfString:@"\n"  withString:@"\\n"];
+    
     return html;
 }
 
@@ -632,7 +632,7 @@ shouldStartLoadWithRequest:(NSURLRequest *)request
 		self.preloadedHTML = html;
 	} else {
 		self.sourceView.text = html;
-		NSString *cleanedHTML = [self removeQuotesFromHTML:self.sourceView.text];
+		NSString *cleanedHTML = [self addSlashes:self.sourceView.text];
 		NSString *trigger = [NSString stringWithFormat:@"zss_editor.setHTML(\"%@\");", cleanedHTML];
 		[self.webView stringByEvaluatingJavaScriptFromString:trigger];
 		
@@ -645,7 +645,7 @@ shouldStartLoadWithRequest:(NSURLRequest *)request
 // Inserts HTML at the caret position
 - (void)insertHTML:(NSString *)html
 {
-    NSString *cleanedHTML = [self removeQuotesFromHTML:html];
+    NSString *cleanedHTML = [self addSlashes:html];
     NSString *trigger = [NSString stringWithFormat:@"zss_editor.insertHTML(\"%@\");", cleanedHTML];
     [self.webView stringByEvaluatingJavaScriptFromString:trigger];
 }
