6cd10a93db75e3a1f131fbf84ce0b062ec06ce70
Diego Rey Mendez <diegoreymendez@gmail.com>
2014-09-20 11:10:29 -0600

    Fixed a problem with the titleDidChange delegate.

--- Classes/WPEditorViewController.m
+++ Classes/WPEditorViewController.m
@@ -1926,7 +1926,7 @@ typedef enum
 }
 
 
-#pragma mark - UITextField Delegate
+#pragma mark - UITextFieldDelegate
 
 - (BOOL)textFieldShouldBeginEditing:(UITextField *)textField
 {
@@ -1950,12 +1950,16 @@ typedef enum
 }
     
 - (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
-{
+{    
     if (textField == self.titleTextField) {
-        if ([self.delegate respondsToSelector: @selector(editorTitleDidChange:)]) {
-            [self.delegate editorTitleDidChange:self];
+        
+        NSString* newTitle = [textField.text stringByReplacingCharactersInRange:range withString:string];
+        
+        if ([self.delegate respondsToSelector: @selector(editorViewController:titleWillChange:)]) {
+            [self.delegate editorViewController:self titleWillChange:newTitle];
         }
     }
+    
     return YES;
 }
 
