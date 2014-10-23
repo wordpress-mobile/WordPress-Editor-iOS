12ebf05cd25093b63dac1efad9d0419f94852f76
Matt Bumgardner <matt@bumgardner.org>
2014-10-23 11:21:23 -0500

    WordPress-iOS-Shared is now set to 0.1.3

--- Example/Pods/Target Support Files/Pods-EditorDemo/Pods-EditorDemo-environment.h
+++ Example/Pods/Target Support Files/Pods-EditorDemo/Pods-EditorDemo-environment.h
@@ -112,7 +112,7 @@
 #define COCOAPODS_POD_AVAILABLE_WordPress_iOS_Shared
 #define COCOAPODS_VERSION_MAJOR_WordPress_iOS_Shared 0
 #define COCOAPODS_VERSION_MINOR_WordPress_iOS_Shared 1
-#define COCOAPODS_VERSION_PATCH_WordPress_iOS_Shared 2
+#define COCOAPODS_VERSION_PATCH_WordPress_iOS_Shared 3
 
 // WordPressCom-Analytics-iOS
 #define COCOAPODS_POD_AVAILABLE_WordPressCom_Analytics_iOS
