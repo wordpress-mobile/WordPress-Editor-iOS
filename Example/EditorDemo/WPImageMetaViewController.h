9f52e8974a22c9cd5cddcbe9966e0f3385777e90
aerych <aerychj@gmail.com>
2014-12-30 15:49:17 -0600

    Documents class purpose.

--- Example/EditorDemo/WPImageMetaViewController.h
+++ Example/EditorDemo/WPImageMetaViewController.h
@@ -9,6 +9,12 @@
 
 @end
 
+/**
+ A view controller that presents a simple form for editing `WPImageMeta` properties.
+ No consideration is given how a change in on property might affect related propreties
+ and only serves to illustrate how changes to WPImageMeta are reflected in the
+ HTML source.
+ */
 @interface WPImageMetaViewController : UIViewController
 
 @property (nonatomic, weak) id<WPImageMetaViewControllerDelegate>delegate;
