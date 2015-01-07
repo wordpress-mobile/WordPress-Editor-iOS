378e71c96bfa9d078a2658b6a5b9475966654351
Matt Bumgardner <matt@bumgardner.org>
2015-01-07 13:41:35 -0600

    Updated shared pod ref to 0.1.6

--- Example/Pods/Target Support Files/Pods-EditorDemo/Pods-EditorDemo-environment.h
+++ Example/Pods/Target Support Files/Pods-EditorDemo/Pods-EditorDemo-environment.h
@@ -79,15 +79,15 @@
 // Please contact the author.
 // Version: 0.2.3.1.
 
-// WordPress-iOS-Shared/Core
-#define COCOAPODS_POD_AVAILABLE_WordPress_iOS_Shared_Core
-#define COCOAPODS_VERSION_MAJOR_WordPress_iOS_Shared_Core 0
-#define COCOAPODS_VERSION_MINOR_WordPress_iOS_Shared_Core 1
-#define COCOAPODS_VERSION_PATCH_WordPress_iOS_Shared_Core 5
+// WordPress-iOS-Shared
+#define COCOAPODS_POD_AVAILABLE_WordPress_iOS_Shared
+#define COCOAPODS_VERSION_MAJOR_WordPress_iOS_Shared 0
+#define COCOAPODS_VERSION_MINOR_WordPress_iOS_Shared 1
+#define COCOAPODS_VERSION_PATCH_WordPress_iOS_Shared 6
 
 // WordPressCom-Analytics-iOS
 #define COCOAPODS_POD_AVAILABLE_WordPressCom_Analytics_iOS
 #define COCOAPODS_VERSION_MAJOR_WordPressCom_Analytics_iOS 0
 #define COCOAPODS_VERSION_MINOR_WordPressCom_Analytics_iOS 0
-#define COCOAPODS_VERSION_PATCH_WordPressCom_Analytics_iOS 16
+#define COCOAPODS_VERSION_PATCH_WordPressCom_Analytics_iOS 17
 
