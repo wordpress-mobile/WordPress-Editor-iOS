8932ebb10c50721a8ded14a06f72bac5365cd4d1
Diego Rey Mendez <diegoreymendez@gmail.com>
2017-02-21 13:03:23 -0300

    Removes the pod folder.

--- Example/Pods/Target Support Files/Pods-EditorDemo/Pods-EditorDemo-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_EditorDemoVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_EditorDemoVersionString[];
-
--- Example/Pods/Target Support Files/WordPressCom-Analytics-iOS/WordPressCom-Analytics-iOS-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "WPAnalytics.h"
-
-FOUNDATION_EXPORT double WordPressComAnalyticsVersionNumber;
-FOUNDATION_EXPORT const unsigned char WordPressComAnalyticsVersionString[];
-
